ef2f4718caa4e29d5a7e03d09a44febdb05f0393
Dopa91 <code.dopatka@gmail.com>
2024-09-21 11:54:07 +0200

    separated.List check √

--- lib/presentation/list_content/separated_list_view_list_content.dart
+++ lib/presentation/list_content/separated_list_view_list_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_beautiful_shopping_list/models/product.dart';
+import 'package:simple_beautiful_shopping_list/presentation/list_entry.dart';
 
 class SeparatedListViewListContent extends StatelessWidget {
   const SeparatedListViewListContent({super.key, required this.products});
@@ -8,7 +9,34 @@ class SeparatedListViewListContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Hier soll eine ListView zurückgegeben werden.
-    throw UnimplementedError();
+    return ListView.separated(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        // return ListTile kann durch "return ListEntry(product: products[index]);"
+        // ersetzt werden. Bzw. einer class für die Darstellung.
+
+        return ListTile(
+          title: Text(products[index].name),
+          leading: const Icon(Icons.access_alarm),
+        );
+      },
+      separatorBuilder: (context, index) => const Divider(),
+    );
   }
 }
+
+// Sobhi
+
+// ListView.separated(
+//       itemBuilder: (BuildContext context, int index) {
+//         return ListEntry(product: products[index]);
+//       },
+//       separatorBuilder: (BuildContext context, int index) {
+//         return const Column(children: [
+//           SizedBox(height: 10),
+//           Divider(color: Colors.red),
+//           SizedBox(height: 10),
+//         ]);
+//       },
+//       itemCount: products.length,
+//     );
